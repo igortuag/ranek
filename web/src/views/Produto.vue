bf1f05b5db58e6447c387282ede1de0b81f8353a
Igor Tuag <igortuag@gmail.com>
2020-11-18 07:12:25 -0300

    [0912] - integration with api

--- web/src/views/Produto.vue
+++ web/src/views/Produto.vue
@@ -1,15 +1,32 @@
 <template>
-  <p>Produto {{ id }}</p>
+  <p>Produto {{ id }} {{ produto }}</p>
 </template>
 
 <script>
+import { api } from "@/services";
+
 export default {
   name: "Produto",
+  data() {
+    return {
+      produto: null
+    };
+  },
   props: {
     id: {
       type: String,
       default: ""
     }
+  },
+  methods: {
+    getProduto() {
+      api.get(`/produto/${this.id}`).then(response => {
+        this.produto = response.data;
+      });
+    }
+  },
+  created() {
+    this.getProduto();
   }
 };
 </script>
