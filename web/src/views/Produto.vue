305e467d6153ba14e39fa614da486619ce0fffc3
Igor Tuag <igortuag@gmail.com>
2020-11-18 07:20:03 -0300

    [0912] - product page structure

--- web/src/views/Produto.vue
+++ web/src/views/Produto.vue
@@ -1,5 +1,21 @@
 <template>
-  <p>Produto {{ id }} {{ produto }}</p>
+  <section>
+    <div v-if="produto" class="produto">
+      <ul class="fotos" v-if="produto.fotos">
+        <li v-for="(foto, index) in produto.fotos" :key="index">
+          <img :src="foto.scr" alt="foto.titulo" />
+        </li>
+      </ul>
+      <div class="info">
+        <h1>{{ produto.nome }}</h1>
+        <p class="preco">{{ produto.preco }}</p>
+        <div class="descricao">{{ produto.descricao }}</div>
+        <button class="btn" v-if="produto.vendido === 'false'">Comprar</button>
+        <button class="btn" v-else disabled>Produto Vendido</button>
+      </div>
+    </div>
+    <PaginaCarregando v-else />
+  </section>
 </template>
 
 <script>
