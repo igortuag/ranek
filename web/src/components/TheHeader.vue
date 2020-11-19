d107665fbdd865fce0722ed33242bf6f8a3e3358
Igor Tuag <igortuag@gmail.com>
2020-11-19 09:36:45 -0300

    [0914] - vuex login

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -4,7 +4,10 @@
       <router-link to="/" class="logo">
         <img src="@/assets/ranek.svg" alt="Ranek" />
       </router-link>
-      <router-link to="/login" class="btn">
+      <router-link v-if="$store.state.login" to="/usuario" class="btn">
+        {{ nome }}
+      </router-link>
+      <router-link v-else to="/login" class="btn">
         Vender / Login
       </router-link>
     </nav>
@@ -12,7 +15,14 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "TheHeader",
+  computed: {
+    nome() {
+      return this.$store.state.usuario.nome.replace(/ .*/, "");
+    }
+  }
+};
 </script>
 
 <style scoped>
