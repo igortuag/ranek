7dad8a9f9b14b0006a8576c8a652e31fd52ef022
Igor Tuag <igortuag@gmail.com>
2020-11-22 09:25:09 -0300

    [0921] - product-styles

--- web/src/views/usuario/UsuarioProdutos.vue
+++ web/src/views/usuario/UsuarioProdutos.vue
@@ -3,18 +3,26 @@
     <h2>Lista de produtos</h2>
     <ProdutoAdicionar />
     <h2>Seus Produtos</h2>
-    {{ usuario_produtos }}
+    <transition-group v-if="usuario_produtos" name="list" tag="ul">
+      <li v-for="(produto, index) in usuario_produtos" :key="index">
+        <ProdutoItem :produto="produto">
+          {{ produto.descricao }}
+        </ProdutoItem>
+      </li>
+    </transition-group>
   </section>
 </template>
 
 <script>
 import ProdutoAdicionar from "@/components/ProdutoAdicionar.vue";
+import ProdutoItem from "@/components/ProdutoItem.vue";
 import { mapState, mapActions } from "vuex";
 
 export default {
   name: "UsuarioProdutos",
   components: {
-    ProdutoAdicionar
+    ProdutoAdicionar,
+    ProdutoItem
   },
   computed: {
     ...mapState(["login", "usuario", "usuario_produtos"])
@@ -22,10 +30,21 @@ export default {
   methods: {
     ...mapActions(["getUsuarioProdutos"])
   },
+  watch: {
+    login() {
+      this.getUsuarioProdutos();
+    }
+  },
   created() {
-    this.getUsuarioProdutos();
+    if (this.login) {
+      this.getUsuarioProdutos();
+    }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+</style>
