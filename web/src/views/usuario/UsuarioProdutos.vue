0eaf5061352526bf1e021244ab5fc586cb17af1f
Igor Tuag <igortuag@gmail.com>
2020-11-22 09:07:43 -0300

    [0921] - store user products and method get

--- web/src/views/usuario/UsuarioProdutos.vue
+++ web/src/views/usuario/UsuarioProdutos.vue
@@ -1,10 +1,30 @@
 <template>
-  <p>Lista de produtos</p>
+  <section>
+    <h2>Lista de produtos</h2>
+    <ProdutoAdicionar />
+    <h2>Seus Produtos</h2>
+    {{ usuario_produtos }}
+  </section>
 </template>
 
 <script>
+import ProdutoAdicionar from "@/components/ProdutoAdicionar.vue";
+import { mapState, mapActions } from "vuex";
+
 export default {
-  name: "UsuarioProdutos"
+  name: "UsuarioProdutos",
+  components: {
+    ProdutoAdicionar
+  },
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"])
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"])
+  },
+  created() {
+    this.getUsuarioProdutos();
+  }
 };
 </script>
 
