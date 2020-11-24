5cbce1716d961e858c798e80b6609b78e705bf9b
Igor Tuag <igortuag@gmail.com>
2020-11-24 07:35:20 -0300

    [0926] - user buy structure component

--- web/src/views/usuario/UsuarioCompras.vue
+++ web/src/views/usuario/UsuarioCompras.vue
@@ -1,10 +1,33 @@
 <template>
-  <p>Usuario Compras</p>
+  <section>
+    <div v-if="compras">
+      <h2>Compras</h2>
+      <div
+        class="produtos-wrapper"
+        v-for="(compras, index) in items"
+        :key="index"
+      >
+        <ProdutoItem v-if="compra.produto" :produto="compra.produto">
+          <p class="vendedor"><span>Vendedor</span>{{ compra.vendedor_id }}</p>
+        </ProdutoItem>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProdutoItem from "@/components/ProdutoItem.vue";
+
 export default {
-  name: "UsuarioCompras"
+  name: "UsuarioCompras",
+  components: {
+    ProdutoItem
+  },
+  data() {
+    return {
+      compras: null
+    };
+  }
 };
 </script>
 
