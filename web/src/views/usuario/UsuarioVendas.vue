feba92fc2e4a54fa44dc60e0549a96f838219429
Igor Tuag <igortuag@gmail.com>
2020-11-24 07:50:21 -0300

    [0926] - sell page structure style and integration

--- web/src/views/usuario/UsuarioVendas.vue
+++ web/src/views/usuario/UsuarioVendas.vue
@@ -1,11 +1,70 @@
 <template>
-  <p>Usuario Vendas</p>
+  <section>
+    <div v-if="vendas">
+      <h2>Vendas</h2>
+      <div
+        class="produtos-wrapper"
+        v-for="(venda, index) in vendas"
+        :key="index"
+      >
+        <ProdutoItem v-if="venda.produto" :produto="venda.produto">
+          <p class="vendedor">
+            <span>Comprador:</span> {{ venda.comprador_id }}
+          </p>
+        </ProdutoItem>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProdutoItem from "@/components/ProdutoItem.vue";
+import { api } from "@/services.js";
+import { mapState } from "vuex";
+
 export default {
-  name: "UsuarioVendas"
+  name: "UsuarioVendas",
+  components: {
+    ProdutoItem
+  },
+  data() {
+    return {
+      vendas: null
+    };
+  },
+  computed: {
+    ...mapState(["usuario", "login"])
+  },
+  methods: {
+    getVendas() {
+      api.get(`/transacao?vendedor_id=${this.usuario.id}`).then(response => {
+        this.vendas = response.data;
+      });
+    }
+  },
+  watch: {
+    login() {
+      this.getVendas();
+    }
+  },
+  created() {
+    if (this.login) {
+      this.getVendas();
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+
+.vendedor span {
+  color: #e80;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+</style>
