b3b3863d87a86f2c35dec18ad6b6ae61e841dbd9
Igor Tuag <igortuag@gmail.com>
2020-11-28 17:01:36 -0300

    [0931] - component ErroNotificacao

---
+++ web/src/components/ErroNotificacao.vue
@@ -0,0 +1,29 @@
+<template>
+  <transition>
+    <ul class="erro" v-if="erro.length > 0">
+      <li v-if="(erro, index) in erros" key="index" v-html="erro"></li>
+    </ul>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "ErroNotificacao",
+  props: {
+    erros: {
+      type: Array
+    }
+  }
+};
+</script>
+
+<style scoped>
+.erro {
+  margin: 20px auto;
+}
+
+.erro li {
+  color: #e80;
+  text-align: center;
+}
+</style>
